08dd4182e57d2406e507dff261338175b843dd7c
deborahsalves <salves.deborah@gmail.com>
2022-06-01 11:04:55 -0300

    Hard-coded db plus button functionality

--- meme-generator/src/components/Meme/Meme.js
+++ meme-generator/src/components/Meme/Meme.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './meme.css';
+import { memeData } from '../../apis/meme-data';
 
 export const Meme = () => {
+    const memeArray = memeData.data.memes;
+    const [currentMeme, setCurrentMeme] = useState(memeArray[Math.floor(Math.random() * memeArray.length)]);
+
+    const handleClick = () => {
+        const rand = Math.floor(Math.random() * memeArray.length);
+        setCurrentMeme(memeArray[rand]);
+    }
     return(
         <main>
-            <form className='meme-form'>
-                <input className='text-input' name="upper-text" id="upper-text"type="text" placeholder="Text on top"></input>
-                <input className='text-input' name="lower-text" id="lower-text" type="text" placeholder="Text on bottom"></input>
-                <button className='form-button'>Get a new meme image 🖼</button>
-            </form>
+            <div className='meme-form'>
+                <input 
+                    className='text-input' 
+                    name="upper-text" 
+                    id="upper-text"
+                    type="text" 
+                    placeholder="Text on top">
+                </input>
+                <input 
+                    className='text-input' 
+                    name="lower-text" 
+                    id="lower-text" 
+                    type="text" 
+                    placeholder="Text on bottom">
+                </input>
+                <button 
+                    className='form-button'
+                    onClick={handleClick}>
+                        Get a new meme image 🖼
+                </button>
+                <div className='image-div'>
+                    <img
+                        className='meme-image'
+                        title={currentMeme.name}
+                        alt={currentMeme.name}
+                        src={currentMeme.url}
+                    />
+                </div>
+            </div>
         </main>
     )
 }
